a6c9779c38e1072f53a8f48c97d4cc5002d2149f
yashas-hm <yashashm.dev@gmail.com>
2024-12-10 13:40:12 +0000

    Workflow Deploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "915166010"
+    serviceWorkerVersion: "1644699795"
   }
 });
